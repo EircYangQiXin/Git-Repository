52e6bfa0fc015318cbf8f9b0ba948eb5bcc732f4
EircYangQiXin <1259998332@qq.com>
2018-08-08 17:39:38 +0800

    This is Test

--- src/com/demo/test/Test01.java
+++ src/com/demo/test/Test01.java
@@ -3,5 +3,7 @@ package com.demo.test;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("This is Test");
+		System.out.println("Why Cann't upload error");
+		System.out.println("Dream");
 	}
 }
